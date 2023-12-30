c3c4452885f7c716306410416e7dd54dc16283b8
Disha Sathavara <69969675+Disha1998@users.noreply.github.com>
2023-12-30 15:37:47 +0530

    Add files via upload

---
+++ Js-practise/Operators.js
@@ -0,0 +1,109 @@
+// Arithmetic Operators
+
+//  +, -, *, /, %, **, ++, --
+
+// Assignment operators
+
+// =, +=, -=, *=, **=, /=, %=
+
+// Comparision operators
+
+// <, >, <=, >=, ==, ===, !=, !==
+
+// example
+// console.log(3 < 2); // true ya false return 
+// console.log(3 > 2);
+// console.log(3 >= 2);
+// console.log(3 <= 2);
+// console.log(3 != 2);
+// console.log(3 !== 2);
+// console.log(3 == 2); 
+// console.log(3 === 2); //strict equal to
+// console.log("2" == 2); //result will be true kyuki woh string ko number me convert kardega, consider karega "2==2"
+// console.log("2" === 2); // result will be false, kyuki woh datatype ko bhi compare karega. 
+
+
+// Logical Operators
+
+// && - And, 
+// || - or, 
+// ! - not
+
+// && 
+// age = 18 && lincense 
+
+// ||
+// comedy || action
+
+// !
+//  red na ho to == !Red
+// green na ho to == !Green
+
+// ==> !Red = to ja sakte ho
+// ==> !Green = to ruk jao
+
+// Ternary Operator
+// ? :
+
+// let budget = 20000
+// let expensivePhone = 25000
+
+
+// (budget >= expensivePhone) ? "Nahi chahie bhai, kyuki budget ke bahar ki cheej hai" : "chalo le lete hai"
+
+// (20000 >= 25000)
+
+
+// operators
+// (a + b)
+// (a - b)
+// (a * b)
+// (a / b)
+// (a % b)
+// (a ** b)
+// (a == b)
+// (a === b)
+
+// examples
+// (3 + 3
+// (4 - 3)
+// (24 / 3)
+// (400 % 3)
+// (4 * 3)
+// (4 == 3)
+// (3 === 3)
+
+// let str1 = "Heloo"
+// let str2 = "Disha"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + "2");
+// console.log(1 + "2");
+
+// console.log("1" + 3 + 2);
+// console.log(1 + 3 + "2");
+
+
+
+
+// ++, --
+
+// ++5 ==> Wrong
+// ++Value ==> right
+
+// ++ => +1
+
+// let gameScore = 98
+// // gameScore ++
+// ++gameScore
+
+// console.log(gameScore);
+
+gameScore = 88
+// gameScore --
+--gameScore
+
+console.log(gameScore);
